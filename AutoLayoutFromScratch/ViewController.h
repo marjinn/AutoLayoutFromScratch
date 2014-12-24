84abe894e071ef1e120cedeab9a749d2a3e61af0
mar Jinn <marjinn@mars-Mac.local>
2014-12-24 20:08:20 +0530

    Initial Commit

---
+++ AutoLayoutFromScratch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutFromScratch
+//
+//  Created by mar Jinn on 12/24/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutFromScratch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutFromScratch
+//
+//  Created by mar Jinn on 12/24/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
